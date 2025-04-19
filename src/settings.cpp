1281fdb8f9e97100f282e657ed82165dc4632692
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2025-04-19 18:41:31 +0100

    test other platforms, zero on my laptop

--- src/settings.cpp
+++ src/settings.cpp
@@ -1,4 +1,5 @@
 #include <Geode/loader/SettingV3.hpp>
+#include <Geode/cocos/platform/win32/CCGL.h>
 #include "hooks/GJBaseGameLayer.hpp"
 
 // shouldnt be necessary but i bet some people will be changing their mod settings
@@ -18,4 +19,27 @@ $on_mod(Loaded) {
             fields->m_resolutionMultiplier = geode::Mod::get()->getSettingValue<double>("resolution-multiplier");
         }
     });
-};
+
+
+    // populate defaults by getting amount of system vram
+    bool hasSetRecommended = geode::Mod::get()->getSavedValue<bool>("has-set-recommended", false);
+    if (hasSetRecommended) return;
+
+    geode::Mod::get()->setSavedValue<bool>("has-set-recommended", true);
+
+    // https://stackoverflow.com/a/5695427
+    // some pretty sketchy glgetinteger calls
+    auto values = new int[4]();
+    glGetIntegerv(GL_GPU_MEMORY_INFO_CURRENT_AVAILABLE_VIDMEM_NVX, values);
+    glGetIntegerv(GL_TEXTURE_FREE_MEMORY_ATI, values);
+
+    if (values[0] == 0) {
+        geode::log::info("No data returned from vram checks :( ({}, {}, {}, {})", values[0], values[1], values[2], values[3]);
+        delete[] values;
+        return;
+    }
+
+    geode::log::info("Video memory checks returned {}", values[0]);
+
+    delete[] values;
+}
